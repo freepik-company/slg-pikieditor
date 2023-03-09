ab6659bffe3be14854e64eb7b7f2c4e2e172b068
JoseLuisRnp <reciojoselu@gmail.com>
2023-03-09 10:51:52 +0100

    proxy server

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    proxy: {
+      "/api": "https://wepik.com",
+    },
+  },
 })
